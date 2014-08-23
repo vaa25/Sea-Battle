b22df8e6e3996ee399d00b8a5839b113e6eb2b5f
Alexander Vlasov <vaa25@yandex.ru>
2014-08-23 18:42:07 +0300

    First commit. Readme added.

---
+++ Main/src/view/DesktopView.java
@@ -0,0 +1,23 @@
+package view;/**
+ * Created with IntelliJ IDEA.
+ * User: Vlasov Alexander
+ * Date: 23.08.2014
+ * Time: 17:55
+ * To change this template use File | Settings | File Templates.
+ * @author Alexander Vlasov
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DesktopView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
