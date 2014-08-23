3e8fe816ce021b394756a6770dc4cbdbbd46c6ad
Alexander Vlasov <vaa25@yandex.ru>
2014-08-23 22:50:48 +0300

    Added javaTima and sobolevstp guys

---
+++ Main/src/net/amboss/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlasov Alexander
+ * Date: 23.08.2014
+ * Time: 22:50
+ * To change this template use File | Settings | File Templates.
+ * @author Alexander Vlasov
+ */
+package net.amboss;
