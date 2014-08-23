7b066b90425775e08a0e53c1fdd3791da2876a75
Alexander Vlasov <vaa25@yandex.ru>
2014-08-23 22:11:32 +0300

    Added javaTima and sobolevstp guys

---
+++ Main/src/model/sobolevstp/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlasov Alexander
+ * Date: 23.08.2014
+ * Time: 22:08
+ * To change this template use File | Settings | File Templates.
+ * @author Alexander Vlasov
+ */
+package model.sobolevstp;
---
+++ Main/src/net/javaTima/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlasov Alexander
+ * Date: 23.08.2014
+ * Time: 22:09
+ * To change this template use File | Settings | File Templates.
+ * @author Alexander Vlasov
+ */
+package net.javaTima;
