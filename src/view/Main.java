d465db9ba6951603ed533cc11458de8058a05684
Alexander Vlasov <vaa25@yandex.ru>
2015-12-25 00:21:21 +0200

    replace anonym with lambda

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,35 +1,28 @@
 package view;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 /**
  * @author Alexander Vlasov
  */
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Sea-Battle.fxml"));
 
         Scene scene = new Scene(root);
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent windowEvent) {
-                System.exit(0);
-            }
-        });
+        stage.setOnCloseRequest(windowEvent -> System.exit(0));
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
 }
